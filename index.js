da9f5649f4a7d2f2f38cc85de76245f376369b90
凉凉 <xyqfer@gmail.com>
2019-10-05 16:59:39 +0800

    v1.0.1: fill index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    getCount: require('./libs/getCount'),
+    getData: require('./libs/getData'),
+    saveData: require('./libs/saveData'),
+    updateData: require('./libs/updateData'),
+};
