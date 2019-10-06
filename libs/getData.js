a828e1388dd1ba8db9b6f1b4aa92ef64d25f859f
凉凉 <xyqfer@gmail.com>
2019-10-06 22:10:08 +0800

    v1.3.0
    依赖修改

--- libs/getData.js
+++ libs/getData.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const AV = require('leanengine');
-const isString = require('lodash/isString');
+const Promise = require('bluebird');
+const _ = require('lodash');
 
-module.exports = async ({ dbName = '', limit = 1000, query = {}, cache = false, }) => {
-    if (!isString(dbName) || dbName === '') {
-        throw 'dbName 不能为空';
-    }
-
+const getData = async ({ dbName, limit, query, }) => {
     const q = new AV.Query(dbName);
 
     if (!query.descending && !query.ascending && !query.addAscending && !query.addDescending) {
@@ -15,7 +12,7 @@ module.exports = async ({ dbName = '', limit = 1000, query = {}, cache = false, 
     }
 
     Object.entries(query).forEach(([ key, params, ]) => {
-        if (isString(params)) {
+        if (_.isString(params)) {
             params = [params];
         }
 
@@ -28,4 +25,38 @@ module.exports = async ({ dbName = '', limit = 1000, query = {}, cache = false, 
     return data.map((item) => {
         return item.toJSON();
     });
+};
+const MAX_PER_TIME = 1000;
+
+module.exports = async ({ dbName = '', limit = MAX_PER_TIME, query = {}, }) => {
+    if (!_.isString(dbName) || dbName === '') {
+        throw 'dbName 不能为空';
+    }
+
+    let skip = 0;
+    if (query.skip) {
+        if (_.isString(query.skip)) {
+            query.skip = [query.skip];
+        }
+        skip = query.skip[0];
+    }
+
+    const times = Math.floor(limit / MAX_PER_TIME);
+    const offsets = _.times(times, _.constant(MAX_PER_TIME));
+    const remaining = limit - times * MAX_PER_TIME;
+    offsets.push(remaining);
+
+    const dbData = await Promise.mapSeries(offsets, async (offset) => {
+        const dbData = await getData({
+            dbName,
+            limit: offset,
+            query,
+        });
+
+        skip += offset;
+        query.skip = [skip];
+        return dbData;
+    });
+
+    return _.flattenDeep(dbData);
 };
